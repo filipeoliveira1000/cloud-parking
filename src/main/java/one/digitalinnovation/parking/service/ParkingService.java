21e9ab5a0f1c33c22291bc231a99a77b12ba4f79
filipeoliveira1000 <filipeoliveira1000@yahoo.com.br>
2022-11-11 13:43:19 -0300

    Adicionando Swagger

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "ALE-1234", "RS", "GOLF", "PRETO");
+        Parking parking1 = new Parking(id1, "ICB-8523", "RS", "FUSCA", "VERDE");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -22,5 +26,17 @@ public class ParkingService {
     }
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", " ");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
